2f3d38df3239a98016091816721ad40b17ed2007
Matej Kuráň <matej.kuran@devsk.io>
2021-10-13 06:36:25 +0200

    basic neural network

---
+++ data_analysis.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#CREATING BALANCED TRAIN DATASET
+def create_balanced_dataset(name):
+    df = pd.read_csv('../../Dataset/2020/' + name + '.csv')
+    malignant = df[df.target == 1]
+    benign = df[df.target == 0]
+    malignant_count = len(malignant)
+    # this gets random benign rows
+    benign = benign.sample(n=malignant_count)
+    # new balanced data
+    new_data = pd.concat([benign, malignant])
+    new_data.to_csv('../../Dataset/2020/balanced_' + name + '.csv')
+
+
+if __name__ == '__main__':
+    #creating balanced dataset
+    #create_balanced_dataset('train')
+    train_df = pd.read_csv('../../Dataset/2020/balanced_train.csv')
+    malignant = train_df[train_df.target == 1]
+    benign = train_df[train_df.target == 0]
+
+
+
---
+++ xor.py
@@ -0,0 +1,40 @@
+from dense import Dense
+from tanh import Tanh
+from losses import mse, mse_prime
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+X = np.reshape([[0, 0], [0, 1], [1, 0], [1, 1]], (4, 2, 1))
+Y = np.reshape([[0], [1], [1], [0]], (4, 1, 1))
+
+epochs = 10000
+learning_rate = 0.1
+
+network = [
+    Dense(2, 3),
+    Tanh(),
+    Dense(3, 1),
+    Tanh()
+]
+
+# train
+for e in range(epochs):
+    error = 0
+    for x, y in zip(X, Y):
+        # forward
+        output = x
+        for layer in network:
+            output = layer.forward(output)
+
+        # error
+        error += mse(y, output)
+
+        # backward
+        grad = mse_prime(y, output)
+        for layer in reversed(network):
+            grad = layer.backward(grad, learning_rate)
+
+    error /= len(X)
+    print('%d/%d, error=%f' % (e + 1, epochs, error))
